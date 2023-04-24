7daa3276f3d16599dba280a20aa10ec6eca2b8d8
Esteban Andrey Gomez A <79619416+unawaretub86@users.noreply.github.com>
2023-04-24 11:11:58 -0500

    Feature/initial scafolding (#3)
    * initial scafolding
    
    * adding architecture
    
    ---------
    
    Co-authored-by: Esteban Gomez <unawaretub@gmail.com>

---
+++ src/main/java/com/example/authenticatorapp/AuthenticatorAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.authenticatorapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticatorAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticatorAppApplication.class, args);
+	}
+
+}
